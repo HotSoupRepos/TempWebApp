771212c76be574de056a41d38a4667686df41d31
Drummer Dee <drummer.deja@gmail.com>
2022-10-19 22:00:59 -0400

    hmm

---
+++ src2/App.js
@@ -0,0 +1,20 @@
+import { FaLinkedin } from 'react-icons/fa';
+import './Styles/Global.css';
+
+
+function App() {
+  return (
+    <>
+      <section className='text'>
+        <h1> Website under construction; will be coming soon! </h1>
+        <h2> Check us on out on our social media page </h2>
+        <div className='socials'>
+          <a href='https://www.linkedin.com/company/hotsoup-info/' target='_blank' rel='noreferrer'><FaLinkedin/></a>
+        </div>
+      </section>
+
+    </>
+  );
+}
+
+export default App;
