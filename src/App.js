5467ea31440c84315579e5a5224cba85e1f9de35
Drummer Dee <drummer.deja@gmail.com>
2022-10-19 23:02:03 -0400

    changes to app & css for placeholder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import './App.css';
+import { FaLinkedin } from 'react-icons/fa';
+import './Global.css';
+
 
 function App() {
   return (
-  )
+    <>
+      <section className='text'>
+        <h1> Website under construction; will be coming soon, thank you! </h1>
+        <h2> Check us on out on our social media page </h2>
+        <div className='socials'>
+          <a href='https://www.linkedin.com/company/hotsoup-info/' target='_blank' rel='noreferrer'><FaLinkedin/></a>
+        </div>
+      </section>
+
+    </>
+  );
 }
 
 export default App;
