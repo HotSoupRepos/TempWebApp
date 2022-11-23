0c6c520a07d190875c542b55ed423834826cf050
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-11-22 20:27:39 -0500

    Remove breaks and made paragraphs

--- src/App.js
+++ src/App.js
@@ -22,12 +22,14 @@ const App = () => {
 						engineering is 100% volunteer based, helping seasoned engineering
 						sharpen their skills, new devs get some real-world experience (to
 						help land that first job that always seems to require experience),
-						all while using their skills to give back to the communities. <br />{" "}
-						<br />
+						all while using their skills to give back to the communities.
+					</p>
+					<p>
 						Our goal is to make an opensource API free for other organizations
 						to use to allow others to utilize what we build to get information
-						out to those who need it the most and get them fed! <br />
-						<br />
+						out to those who need it the most and get them fed!
+					</p>
+					<p>
 						Please be patient while we build the mobile app. Also, keep an eye
 						out for another iteration of the website, which will showcase more
 						details, all of our INCREDIBLE volunteers and links to download the
